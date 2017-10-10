23960442cac7fd776dc3f98f99435493fb2b178b
Johan Ferreras <jferrer4@cs.binghamton.edu>
2017-10-09 22:19:51 -0400

    Assignment 3

---
+++ CA3jferrer4/1CA3/FBLPostLL.h
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdlib.h>
+#include "string"
+
+using namespace std;
+
+class FBLPostLL{
+	public:
+		FBLPost post;
+		Node *next;
+
+}
---
+++ CA3jferrer4/1CA3/FBLUser.h
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stdlib.h>
+#include "string"
+
+using namespace std;
+
+class FBLUser {
+	private:
+		//data
+		string userID;
+		string password;
+		string first;
+		string last;
+		
+		LinkedList 
+	
+	public:
+
+}
